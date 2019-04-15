1e9fbcd4bcee7ef2cb6f2393eb5eb9a7ad209a97
LesLiu94 <LesLiu94@users.noreply.github.com>
2019-04-15 00:53:43 -0400

    fixed syntax errors and specified schema location for table creation

--- makeEmployeesSchema.sql
+++ makeEmployeesSchema.sql
@@ -1,27 +1,27 @@
 do $$ begin
 
-drop schema if exists 'employeesdb' cascade
-create schema 'employeesdb'
+drop schema if exists employeesdb cascade;
+create schema employeesdb;
 	
 if not exists (select 1 from pg_catalog.pg_type where typname = 'sex') then
 	create type sex as enum ('M','F');
 end if;
 
-create table departments (
+create table employeesdb.departments (
 	dept_no char(4) primary key,
 	dept_name varchar(40) unique not null
 );
 
-create table employees (
+create table employeesdb.employees (
 	emp_no integer unique not null,		--shows primary key symbol but not listed under "Indexes"
 	birth_date date not null,
 	first_name varchar(14) not null,
 	last_name varchar(16) not null,
-	gender emp_gender not null,
+	gender sex not null,
 	hire_date date not null
 );
 
-create table dept_emp (
+create table employeesdb.dept_emp (
 	emp_no integer references employees(emp_no) on update cascade on delete restrict,
 	dept_no char(4) references departments(dept_no) on update cascade on delete restrict,
 	from_date date not null,
@@ -29,7 +29,7 @@ create table dept_emp (
 	primary key (emp_no, dept_no)
 );
 
-create table dept_manager (
+create table employeesdb.dept_manager (
 	dept_no char(4) references departments(dept_no) on update cascade on delete restrict,
 	emp_no integer references employees(emp_no) on update cascade on delete restrict,
 	from_date date not null,
@@ -37,14 +37,14 @@ create table dept_manager (
 	primary key (emp_no, dept_no)
 );
 
-create table salaries (
+create table employeesdb.salaries (
 	emp_no integer primary key references employees(emp_no) on update cascade on delete restrict,
 	salary integer not null,
 	from_date date not null, 		--shows primary key symbol but not listed under "Indexes"
 	to_date date not null
 );
 
-create table titles (
+create table employeesdb.titles (
 	emp_no integer primary key references employees(emp_no) on update cascade on delete restrict,
 	title varchar(50) not null,		--shows primary key symbol but not listed under "Indexes"
 	from_date date not null,		--shows primary key symbol but not listed under "Indexes"
