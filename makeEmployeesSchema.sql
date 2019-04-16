e996ceaeb93600111ecbe81ab162323dcf1561ec
LesLiu94 <LesLiu94@users.noreply.github.com>
2019-04-16 16:41:25 -0400

    changed integer data type to numeric(11). created indexes for the tables.

--- makeEmployeesSchema.sql
+++ makeEmployeesSchema.sql
@@ -12,8 +12,10 @@ create table employeesSchema.departments (
 	dept_name varchar(40) unique not null
 );
 
+create index dept_name on employeesSchema.departments(dept_name);
+
 create table employeesSchema.employees (
-	emp_no integer unique not null,		--shows primary key symbol but not listed under "Indexes"
+	emp_no numeric(11) unique not null,		--shows primary key symbol but not listed under "Indexes"
 	birth_date date not null,
 	first_name varchar(14) not null,
 	last_name varchar(16) not null,
@@ -22,31 +24,43 @@ create table employeesSchema.employees (
 );
 
 create table employeesSchema.dept_emp (
-	emp_no integer references employeesSchema.employees(emp_no) on update cascade on delete restrict,
+	emp_no numeric(11) references employeesSchema.employees(emp_no) on update cascade on delete restrict,
 	dept_no char(4) references employeesSchema.departments(dept_no) on update cascade on delete restrict,
 	from_date date not null,
 	to_date date not null,			--not sure why this cannot be null
 	primary key (emp_no, dept_no)
 );
+
+create index emp_no_dept_emp_index on employeesSchema.dept_emp(emp_no);
+
+create index dept_no_dept_emp_index on employeesSchema.dept_emp(dept_no);
 
 create table employeesSchema.dept_manager (
 	dept_no char(4) references employeesSchema.departments(dept_no) on update cascade on delete restrict,
-	emp_no integer references employeesSchema.employees(emp_no) on update cascade on delete restrict,
+	emp_no numeric(11) references employeesSchema.employees(emp_no) on update cascade on delete restrict,
 	from_date date not null,
 	to_date date not null,			--not sure why this cannot be null
 	primary key (emp_no, dept_no)
 );
 
+create index emp_no_dept_manager_index on employeesSchema.dept_manager(emp_no);
+
+create index dept_no_dept_manager_index on employeesSchema.dept_manager(dept_no);
+
 create table employeesSchema.salaries (
-	emp_no integer primary key references employeesSchema.employees(emp_no) on update cascade on delete restrict,
-	salary integer not null,
+	emp_no numeric(11) primary key references employeesSchema.employees(emp_no) on update cascade on delete restrict,
+	salary numeric(11) not null,
 	from_date date not null, 		--shows primary key symbol but not listed under "Indexes"
 	to_date date not null
 );
 
+create index emp_no_salaries_index on employeesSchema.salaries(emp_no);
+
 create table employeesSchema.titles (
-	emp_no integer primary key references employeesSchema.employees(emp_no) on update cascade on delete restrict,
+	emp_no numeric(11) primary key references employeesSchema.employees(emp_no) on update cascade on delete restrict,
 	title varchar(50) not null,		--shows primary key symbol but not listed under "Indexes"
 	from_date date not null,		--shows primary key symbol but not listed under "Indexes"
 	to_date date
 );
+
+create index emp_no_titles_index on employeesSchema.titles(emp_no);
