9917c5c69e3792ecc59b23c015ff53b2a4a3ca0e
LesLiu94 <LesLiu94@users.noreply.github.com>
2019-10-22 22:48:01 -0400

    WIP working on sorting employee list by first name and if they are the same, sort by last name. also need to incorporate pagination to employee list.

--- frontend/app/scripts/controllers/employee-list-ctrl.js
+++ frontend/app/scripts/controllers/employee-list-ctrl.js
@@ -10,5 +10,10 @@
 angular.module('employeeProjectApp')
   .controller('EmployeeListCtrl', ['$scope', 'employeeList', function ($scope, employeeList) {
     $scope.title = "Employee List";
-    $scope.employeeList = employeeList;
+    $scope.employeeList = employeeList.sort(function(a,b)
+    { 
+      if(a.firstName.localeCompare(b.firstName)==0){
+        return a.lastName.localeCompare(b.lastName);
+      }
+      return a.firstName.localeCompare(b.firstName)});
   }]);
