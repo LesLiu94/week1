9e16a8fff431c1d1b77809c53e38cfdbd9630753
LesLiu94 <LesLiu94@users.noreply.github.com>
2019-11-18 18:00:31 -0500

    getting null pointer exception on either toDate or fromDate when adding an employee

--- frontend/app/scripts/controllers/add-employee-ctrl.js
+++ frontend/app/scripts/controllers/add-employee-ctrl.js
@@ -8,9 +8,37 @@
  * Controller of the employeeProjectApp
  */
 angular.module('employeeProjectApp')
-  .controller('AddEmployeeCtrl', ['$scope', function ($scope) {
+  .controller('AddEmployeeCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.title = "Add Employee";
-    
+    $scope.addEmployeeForm = {
+      fname: '',
+      lname: '',
+      dob: '',
+      title: '',
+      salary: '',
+      hdate: '',
+      gender: '',
+      fdate: '',
+      tdate: ''
+    }
+    $scope.addEmployee = function(addEmployeeCriteria) {
+      $http({
+        method: 'POST',
+        url: "http://localhost:8080/api/AddEmployee/employee",
+        data: {
+            departments: "",
+            firstName: addEmployeeCriteria.firstName,
+            lastName: addEmployeeCriteria.lastName,
+            dob: addEmployeeCriteria.dob,
+            employeeTitle: addEmployeeCriteria.employeeTitle,
+            salary: addEmployeeCriteria.salary,
+            hireDate: addEmployeeCriteria.hdate,
+            gender: addEmployeeCriteria.gender,
+            fromDate: addEmployeeCriteria.fdate,
+            toDate: addEmployeeCriteria.tdate
+        }
+      })
+    }
   }]);
   
 
